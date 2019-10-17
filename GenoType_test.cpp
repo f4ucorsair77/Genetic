cc63d11ea1d2d072bd4b4a68b169e72530a276a8
f4ucorsair77 <brendan.oconnor@emergentspace.com>
2019-10-17 14:24:45 -0500

    Updated the build to use cmake on a Docker image

--- GenoType_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * GenoType_test.cpp
- *
- *  Created on: Dec 13, 2017
- *      Author: devel
- */
-
-#include "GenoType.h"
-
---
+++ UnitTest/GenoType_test.cpp
@@ -0,0 +1,15 @@
+/*
+ * GenoType_test.cpp
+ *
+ *  Created on: Dec 13, 2017
+ *      Author: devel
+ */
+#include <iostream>
+#include "GenoType.h"
+#include "population.h"
+
+int main(int argc, char const *argv[])
+{
+  std::cout << "Hello Whirled from Docker container!!" << std::endl;
+  return 0;
+}
